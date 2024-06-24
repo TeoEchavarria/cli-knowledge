2d9819e4eab32840c35ee0b51b3e7a7d4dfedd99
TeoEchavarria <echavarria.0042@gmail.com>
2024-06-23 19:44:50 -0500

    Create Note

--- cli/actions/create_note.py
+++ cli/actions/create_note.py
@@ -1,2 +1,36 @@
+from notion.client import create
+from utils.cli import open_file
+
 def run(args):
-    print("create_note")
+    content = open_file()
+    new_note_properties = {
+    "Title": {
+        "title": [
+            {
+                "text": {
+                    "content": args["title"]
+                }
+            }
+        ]
+    },
+    "List": {
+        "select": {
+            "name" :  args["list"]
+         } if args["list"] != None else None
+    },
+    "Categories": {
+        "multi_select": [
+            {"name" : category} for category in args["categories"].split(",")
+        ] if args["categories"] != None else []
+    },
+    # "Notes": {
+    #     "relation": [
+    #         {
+    #             "name" : note_id for note_id in args["notes"].split(",") 
+    #         } if args["notes"] != None else {}
+    #     ]
+    # }
+    # Añade más propiedades según la estructura de tu base de datos
+}
+    # Añade la tarea a la base de datos
+    new_note = create("note", new_note_properties, content)
--- config/json.py
+++ config/json.py
@@ -21,9 +21,10 @@ yagami = [
             "alias": "r",
             "description": "Read a task",
             "attributes": [
-              {"name": "title", "alias":"t", "type": str, "required": True, "description": "Title of the task"},
-              {"name": "category", "alias":"c", "type": str, "required": False, "description": "Category associated with the task"},
-              {"name": "notes",  "alias":"n", "type": str, "required": False, "description": "Note ID to link to the task"},
+              {"name": "title", "alias":"t", "type": str, "required": True, "description": "Title of the task to read"},
+              {"name": "list", "alias":"l", "type": str, "required": False, "description": "List associated with the task to read"},
+              {"name": "category", "alias":"c", "type": str, "required": False, "description": "Category associated with the task to read"},
+              {"name": "notes",  "alias":"n", "type": str, "required": False, "description": "Note ID to link to the task to read"},
               {"name": "task_id", "alias":"ti", "type": str, "required": False, "description": "ID of the task to read"}
             ]
           },
@@ -42,6 +43,7 @@ yagami = [
             "description": "Delete a task",
             "attributes": [
               {"name": "task_id","alias":"ti", "type": str, "required": False, "description": "ID of the task to delete"},
+              {"name": "list", "alias":"l", "type": str, "required": False, "description": "List associated with the task to delete"},
               {"name": "categories", "alias":"c","type": str, "required": False, "description": "List of categories of the task to delete"}
             ]
           }
@@ -58,6 +60,7 @@ yagami = [
             "description": "Create a new note",
             "attributes": [
               {"name": "title", "alias":"t","type": str, "required": True, "description": "Title of the note"},
+              {"name": "list", "alias":"l", "type": str, "required": False, "description": "List associated with the note"},
               {"name": "categories", "alias":"c","type": str, "required": False, "description": "List of Categories associated with the note"},
               {"name": "linked_notes","alias":"ln", "type": str, "required": False, "description": "List of note IDs this note links to"},
               {"name": "linked_tasks", "alias":"lt","type": str, "required": False, "description": "List of task IDs this note links to"}
@@ -68,8 +71,9 @@ yagami = [
             "alias": "r",
             "description": "Read a note",
             "attributes": [
-              {"name": "title", "alias":"t", "type": str, "required": True, "description": "Title of the note"},
-              {"name": "category", "alias":"c", "type": str, "required": False, "description": "Category associated with the note"},
+              {"name": "title", "alias":"t", "type": str, "required": True, "description": "Title of the note to read"},
+              {"name": "list", "alias":"l", "type": str, "required": False, "description": "List associated with the note to read"},
+              {"name": "category", "alias":"c", "type": str, "required": False, "description": "Category associated with the note to read"},
               {"name": "note_id", "alias":"ti", "type": str, "required": False, "description": "ID of the note to read"}
             ]
           },
@@ -80,8 +84,6 @@ yagami = [
             "attributes": [
               {"name": "note_id", "alias":"ni","type": str, "required": True, "description": "ID of the note to update"},
               {"name": "categories", "alias":"c","type": str, "required": False, "description": "Updated list of Categories associated with the note"},
-              {"name": "linked_notes", "alias":"ln","type": str, "required": False, "description": "Updated list of linked note IDs"},
-              {"name": "linked_tasks","alias":"lt", "type": str, "required": False, "description": "Updated list of task IDs this note links to"}
             ]
           },
           {
@@ -90,6 +92,7 @@ yagami = [
             "description": "Delete a note",
             "attributes": [
               {"name": "note_id", "alias":"ni", "type": str, "required": True, "description": "ID of the note to delete"},
+              {"name": "list", "alias":"l", "type": str, "required": False, "description": "List associated with the note to delete"},
               {"name": "categories", "alias":"c", "type": str, "required": False, "description": "List of categories of the note to delete"}
             ]
           }
